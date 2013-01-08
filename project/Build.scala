557aa21e29b2f75e00dd255a9cc8e8415dffde2a
yuichirokataoka <p-kataoka@cs-tech.jp>
2013-01-07 22:24:39 -0800

    Signed-off-by: yuichirokataoka <p-kataoka@cs-tech.jp>

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,17 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+		  "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+	      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
+	      "org.scalatest" %% "scalatest" % "1.7.1" % "test",
+	      "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
+     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+          testOptions in Test := Nil,
+          resolvers ++= Seq("repo.novus snaps" at "http://repo.novus.com/snapshots/",
+          "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+          "Sonatype OSS groups" at "https://oss.sonatype.org/content/groups/scala-tools/")
     )
 
 }
