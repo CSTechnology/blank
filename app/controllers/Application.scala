b782dbcaea102fa6119eb07e2fd9cc846545cf39
kengo1119jp <kengo1119jp@192.168.90.4>
2013-02-17 19:45:39 +0900

    画像表示

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import play.api.Play.current
 import play.api.mvc._
 
 object Application extends Controller {
@@ -9,5 +10,13 @@ object Application extends Controller {
     Ok(views.html.index(getTitle()))
   }
   
+  def file = Action{
+    val app = Play.application
+    var file = new java.io.File("/Users/kengo1119jp/Documents/workspace/blank/app/data/favicon.png")
+    val source = scala.io.Source.fromFile(file)(scala.io.Codec.ISO8859)
+    val byteArray = source.map(_.toByte).toArray
+    source.close()
+    Ok(byteArray).as("image/png")
+  }
   def getTitle() = "Your new blank application is ready."
 }
